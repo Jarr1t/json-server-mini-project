c83d9382cb0c53b4d9fb8cdb39ad21c2618fbc41
Jarr1t <aliceajarrit@gmail.com>
2021-03-01 14:43:15 -0500

    fixed a lot of stuff

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const cardCont = document.querySelector("#collection");
     fetchCall(cardCont);
@@ -21,8 +18,10 @@ function fetchCall (cardCont){
 }
 
 function newCard(lang, container) {
-    
     const button = document.createElement("button");
+    const updateButton = document.createElement("button")
+    updateButton.innerText = "Update"
+    updateButton.class = "btn"
     button.id = lang.name;
     button.class = "btn";
     button.innerText = "Remove";
@@ -33,12 +32,90 @@ function newCard(lang, container) {
         <h2>${lang.name}</h2>
         <img alt="${lang.name}" src="${lang.image}" class="avatar" />
     `;
+
     newCard.append(button);
     container.append(newCard);
     button.addEventListener("click", (e) => {
         const parent = e.target.parentElement.id;
         deleteLang(parent);
     });
+    newCard.append(updateButton)
+    updateButton.addEventListener("click", (e) => {
+        const parent = e.target.parentElement
+        let updateForm = document.createElement("form")
+        updateForm.className = "add-form"
+        updateForm.innerHTML = `
+        <h3>Change Language</h3>
+
+        <input
+          id ="name"
+          required = "true"
+          type="text"
+          name="name"
+          value=""
+          placeholder="Enter new Language"
+          class="input-text"
+        />
+        <br />
+        <input
+          id = "image"
+          type="text"
+          name="image"
+          value=""
+          placeholder="Enter new image"
+          class="input-text"
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="update"
+          class="submit"
+        />
+    `
+    parent.innerHTML = ""
+    parent.append(updateForm)
+    updateForm.addEventListener("submit", (e) => {
+        let parentId = e.target.parentElement.id;
+        updateLanguage(e, parentId)})
+    })
+}
+
+function updateLanguage(e, id){
+    e.preventDefault()
+    let newName = e.target[0].value
+    let newImage = e.target[1].value
+    options = {
+        method : "PATCH",
+        headers : {
+            "Content-Type": "application/json"
+        },
+        body : JSON.stringify({
+            "name" : newName,
+            "image" : newImage
+        })
+    }
+    fetch(`http://localhost:3000/languages/${id}`, options)
+        .then(response => response.json())
+        .then( data => {
+            updateCard(data, id)})
+}
+
+function updateCard(data, id){
+    let currentId = document.getElementById(id)
+    const button = document.createElement("button");
+    const updateButton = document.createElement("button")
+    updateButton.innerText = "Update"
+    updateButton.class = "btn"
+    button.id = data.name;
+    button.class = "btn";
+    button.innerText = "Remove";
+    console.log(data)
+    currentId.innerHTML = `
+        <h2>${data.name}</h2>
+        <img alt="${data.name}" src="${data.image}" class="avatar" />
+    `;
+    currentId.append(button, updateButton)
 }
 
 function deleteLang(id) {
@@ -72,7 +149,6 @@ function addLang(){
     .then(data => {
         const container = document.querySelector("#collection");
         newCard(data, container);
-        debugger;
     });
 }
 
